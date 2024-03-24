2ee10a33b88876c35fab2c78f13b175424065cc2
Acusor <agriseagrisoare19@gmail.com>
2024-03-24 17:40:16 +0200

    Add project files.

---
+++ PMS/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace proiect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           /* Masinitza[] garaj = new Masinitza[3];
+            Masinitza mas1 = new Masinitza("Mustang");
+            Masinitza mas2 = new Masinitza("Dodge");
+            Masinitza mas3 = new Masinitza("Camaro");
+            garaj[0] = mas1;
+            garaj[1] = mas2;
+            garaj[2] = mas3;
+           foreach(Masinitza mas in garaj)
+            {
+                Console.WriteLine(mas.model);
+            }*/
+
+
+
+            Masinitza[] garaj = { new Masinitza("Mustang"), new Masinitza("Dodge"),new Masinitza("Camaro") };
+            foreach(Masinitza mas in garaj)
+            {
+                Console.WriteLine(mas.model);
+            }
+        }
+        
+    }
+
+    class Masinitza
+    {
+        public String model;
+        public Masinitza(string model)
+        {
+            this.model = model;
+        }
+
+    }
+}
