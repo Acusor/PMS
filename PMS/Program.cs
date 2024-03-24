867724977facf79a5b5b627f97bcc3e3b5ebf754
Acusor <agriseagrisoare19@gmail.com>
2024-03-24 21:26:22 +0200

    clasele declarate

--- PMS/Program.cs
+++ PMS/Program.cs
@@ -4,34 +4,111 @@ namespace proiect
 {
     class Program
     {
-        static void Main(string[] args)
+
+        class Sistem
         {
-           /* Masinitza[] garaj = new Masinitza[3];
-            Masinitza mas1 = new Masinitza("Mustang");
-            Masinitza mas2 = new Masinitza("Dodge");
-            Masinitza mas3 = new Masinitza("Camaro");
-            garaj[0] = mas1;
-            garaj[1] = mas2;
-            garaj[2] = mas3;
-           foreach(Masinitza mas in garaj)
+
+        }
+        class Medicamente
+        {
+            public int id { get; set; }
+            public string nume { get; set; }
+
+            public string categorie { get; set; }
+            public string descriere { get; set; }
+            public double pret { get; set; }
+            public int cantitate { get; set; }
+            public string dataexpirare{get;set;}
+
+           
+        }
+        class Furnizor
+        {
+
+        }
+        class Inventar
+        {
+
+        }
+        class BazaDeDate
+        {
+
+        }
+        class Angajat
+        {
+
+        }
+        class Client
+        {
+
+        }
+        class Signin
+        {
+
+        }
+
+        public static void Main(string[] args)
+        {
+            while (true)
             {
-                Console.WriteLine(mas.model);
-            }*/
+                Console.WriteLine("**Aplicatie de gestiune a unei farmacii");
+                Console.WriteLine("1.Citire date de la tastatura");
+                Console.WriteLine("2.Afisarea datelor de la tastatura");
+                Console.WriteLine("3.Cautarea dupa anumite criterii");
+                string o = Console.ReadLine();
+                switch (o)
+                {
+                    case "1":
+                        Console.WriteLine("test");
+                        CitireTastatura();
+                        break;
+
+                    case "2":
+                        Console.WriteLine("test2");
+                        break;
+
+                    case "3":
+                        Console.WriteLine("test3");
+                        break;
+                }
+            }
+        }
 
 
 
-            Masinitza[] garaj = { new Masinitza("Mustang"), new Masinitza("Dodge"),new Masinitza("Camaro") };
-            foreach(Masinitza mas in garaj)
+
+
+
+
+        static void CitireTastatura()
+        {
+            Console.WriteLine("Introduceti medicamentul:");
+            string m;
+            while((m=Console.ReadLine())!="atat")
             {
-                Console.WriteLine(mas.model);
+                string[] listamedicamente = m.Split(',');
+                Medicamente medicament = new Medicamente
+                {
+                    id = int.Parse(listamedicamente[0]),
+                    nume = listamedicamente[1],
+                    descriere = listamedicamente[2],
+                    pret = double.Parse(listamedicamente[3]),
+                    cantitate = int.Parse(listamedicamente[4]),
+                    dataexpirare =(listamedicamente[5])
+
+                };
             }
 
-            Console.WriteLine("testez");
-        }
-        
-    }
 
-    class Masinitza
+            
+           
+
+        }
+
+    }
+}
+
+    /*class Masinitza
     {
         public String model;
         public Masinitza(string model)
@@ -39,5 +116,4 @@ namespace proiect
             this.model = model;
         }
 
-    }
-}
+    }*/
